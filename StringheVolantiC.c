3a0167a681e6582b9e3a4ad242a7c8cba9b24771
Billie99 <billiearmstrong1886@gmail.com>
2017-05-24 11:30:57 +0200

    Programma completato

--- StringheVolantiC.c
+++ StringheVolantiC.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
 #include <time.h>
 #include <ncurses.h>
 
 int main () {
 
 struct timespec intervallo;
-intervallo.tv_sec = 3;
-intervallo.tv_nsec = 700 / 1000;
+intervallo.tv_sec = 0;
+intervallo.tv_nsec = 700 * 1000 * 1000;
 
 initscr();
 
@@ -14,15 +13,15 @@ curs_set(0);
 
 mvaddstr(15, 4, "Hello World!");
 nanosleep(&intervallo, NULL);
+refresh();
 mvaddstr(10, 20, "How are you?");
 nanosleep(&intervallo, NULL);
+refresh();
 mvaddstr(5, 4, "I'm Ok");
-
+nanosleep(&intervallo, NULL);
 refresh();
 
 getch();
-
 endwin();
-
 return 0;
 } 
